a21d58b0f6fcbe3790bc8cb3f8b6077c59c3a8f4
Zhang <zhang@i22-yzhang.local>
2022-08-06 18:47:22 +0900

    fix bugs

--- exec.sh
+++ exec.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 APPDIR=`dirname $0`
-python -u $APPDIR/main_qm9.py --num_workers 2 --lr 5e-4 --property alpha --exp_name exp_1_alpha --epochs 10 --outf /mnt/nfs-mnj-hot-01/tmp/i22_yzhang/egnn/qm9/logs
+python -u $APPDIR/main_qm9.py --num_workers 2 --lr 5e-4 --property alpha --exp_name exp_1_alpha --epochs 100 --outf /mnt/nfs-mnj-hot-01/tmp/i22_yzhang/egnn/qm9/logs
